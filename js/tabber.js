f16e2e559383f485001cb24bd43fd9342238e51f
Brian Dominick <badominick@gmail.com>
2021-05-29 09:30:08 -0400

    Convert Asciidoctor-rendered HTML into form needed for Bootstrap navtab-panels

--- js/tabber.js
+++ js/tabber.js
@@ -2,116 +2,106 @@
 layout: null
 ---
 // Constants.
-const tabsDict = {{ site.features.actions.versioning.tabbers | jsonify }}
+const tabsDict = {{ site.features.actions.versioning.tabsets | jsonify }}
 
 // Variables.
-var selectingTabs = false;
+// var selectingTabs = false;
+//
+// /**
+//  * Process a tab click and activate all the tabs related to the clicked one.
+//  */
+// function tabClicked(tabLink) {
+//     if (selectingTabs)
+//       return;
+//
+//     selectingTabs = true;
+//
+//     // Get all the tab links.
+//     var tabsLinks= document.querySelectorAll('.nav-tabs a');
+//     // Get all the tab items.
+//     var tabs = document.querySelectorAll('.tab-item');
+//     // Get the class of the tabs to be shown.
+//     var tabsClass = tabLink.className.replace('link-', '');
+//
+//     // Activate all the tab links.
+//     for(i = 0; i < tabsLinks.length; i++) {
+//       var newTabLink = tabsLinks[i];
+//       var tabParent = newTabLink.parentNode;
+//       if (tabLink.className == newTabLink.className
+//         && tabParent.className.indexOf("active") === -1)
+//         tabParent.className += " active";
+//       else if (tabLink.className != newTabLink.className)
+//         tabParent.className = tabParent.className.replace(/\bactive\b/g, "");
+//     }
+//
+//     // Activate all the tabs.
+//     for(i = 0; i < tabs.length; i++) {
+//       var newTab = tabs[i];
+//       if (tabLink.hash.indexOf(newTab.name) !== -1) {
+//         newTab.className += " in";
+//         newTab.className += " active";
+//       } else {
+//         newTab.className = newTab.className.replace(/\bin\b/g, "");
+//         newTab.className = newTab.className.replace(/\bactive\b/g, "");
+//       }
+//     }
+//     selectingTabs = false;
+// };
 
 /**
- * Process a tab click and activate all the tabs related to the clicked one.
- */
-function tabClicked(tabLink) {
-    if (selectingTabs)
-      return;
-
-    selectingTabs = true;
-
-    // Get all the tab links.
-    var tabsLinks= document.querySelectorAll('.nav-tabs a');
-    // Get all the tab items.
-    var tabs = document.querySelectorAll('.tab-item');
-    // Get the class of the tabs to be shown.
-    var tabsClass = tabLink.className.replace('link-', '');
-
-    // Activate all the tab links.
-    for(i = 0; i < tabsLinks.length; i++) {
-      var newTabLink = tabsLinks[i];
-      var tabParent = newTabLink.parentNode;
-      if (tabLink.className == newTabLink.className
-        && tabParent.className.indexOf("active") === -1)
-        tabParent.className += " active";
-      else if (tabLink.className != newTabLink.className)
-        tabParent.className = tabParent.className.replace(/\bactive\b/g, "");
-    }
-
-    // Activate all the tabs.
-    for(i = 0; i < tabs.length; i++) {
-      var newTab = tabs[i];
-      if (tabLink.hash.indexOf(newTab.name) !== -1) {
-        newTab.className += " in";
-        newTab.className += " active";
-      } else {
-        newTab.className = newTab.className.replace(/\bin\b/g, "");
-        newTab.className = newTab.className.replace(/\bactive\b/g, "");
-      }
-    }
-    selectingTabs = false;
-};
-
-/**
- * Convert the div blocks configured with 'tabber-panel' class into Bootstrap
- * tabs structures so they are displayed as tab controls.
+ * Convert the div blocks configured with 'tabber-' classes into Bootstrap
+ * tabs structures so they can be treated as Bootstrap tab controls.
  *
  * For more info: https://www.w3schools.com/bootstrap/bootstrap_tabs_pills.asp
  */
-$(function() {
+$( document ).ready(function() {
   // Get all the tabbed blocks of the page.
-  $(".tabber-panel").each(function() {
+  $(".tabber-tabset").each(function() {
+    $(this).addClass("container")
     var classes = $(this).attr("class").split(" ");
-    classes.forEach(function callback(value, index) {
-      tabberName = value.match(/^tabs-([a-z_\-]+)/)
-      console.log(tabberName)
-    });
-    // Get the content div of each tabbed block.
-    $(this).children("td.openblock > .content").each(function() {
-
-      // Set the proper class to the content so it is processed as a tabs container.
-      $(this).className += " tabber-content";
-
-      // Create the tabs panel control.
-      var tabTitles = document.createElement('ul');
-      tabTitles.className += " nav";
-      tabTitles.className += " nav-tabs";
-      // Process all the tabs for the tabber panel.
-      var tabDivs = tabContent.getElementsByClassName("tabber-item");
-      for (j = tabDivs.length - 1; j >= 0; j--) {
-        // Update style of each tab content div.
-        var tabDiv = tabDivs[j];
-        tabDiv.className += " tab-pane";
-        tabDiv.className += " fade";
-        if (j == tabDivs.length - 1) {
-            tabDiv.className += " in";
-            tabDiv.className += " active";
-        }
-
-        // Get the name of the tab.
-        var tabClassName = tabDiv.className.split(" ")[2];
-        var tabName = tabsDict[tabClassName];
-        if (tabName == null)
-            tabName = tabClassName.split('_').join(' ');
-
-        // Create the corresponding tab control for the tab content.
-        var tabTitle = document.createElement('li');
-        if (j == tabDivs.length - 1)
-            tabTitle.className += " active";
-        // Create the link for the tab control.
-        var tabLink = document.createElement('a');
-        tabLink.className = "link-" + tabClassName;
-        tabLink.href = "#" + tabClassName;
-        tabDiv.name = tabClassName;
-        tabLink.onclick = function () { tabClicked(this); };
-        var tabText = document.createTextNode(tabName);
-        tabLink.appendChild(tabText);
-
-        // Add the link to the tab control.
-        tabTitle.appendChild(tabLink);
-
-        // Add the tab control to the tabs panel.
-        tabTitles.appendChild(tabTitle);
+    for (let i = 0; i < classes.length; ++i) {
+      if (classes[i] !== null) {
+        if (classes[i].match(/^tabs-([a-z_\-]+)/)) {
+          var tabberName = classes[i].replace("tabs-","");
+          break;
+        };
       };
-
-      // Insert the tabs panel control above the tab content div.
-      //tabbedElements[i].insertBefore(tabTitles, tabContent);
+    };
+    var tabSet = tabsDict[tabberName]['swap']['opts'];
+    var theseTabs = [];
+    // Loop through the tabs determining name and contents
+    $(this).prepend('<ul class="nav nav-tabs" role="tablist"></ul>')
+    // Get each tabbed panel
+    $(this).find('.tabber-item').each(function() {
+      var classes = $(this).attr("class").split(" ");
+      for (let i = 0; i < classes.length; ++i) {
+        if (classes[i] !== null) {
+          if (classes[i].match(/^tab-([a-z_\-]+)/)) {
+            var tabName = classes[i].replace("tab-","");
+            break;
+          };
+        };
+      };
+      if (tabSet !== null) {
+        // use a match from the tabSet to affix labevar tabLabel = ;
+        var tabLabel = tabSet.find(opt => {
+          return opt.slug == tabName
+        }).text;
+      };
+      $(this).wrap('<div class="tab-pane ' + tabName + '-pane fade" role="tabpanel"></div>')
+      // use the caption IF there is one
+      $(this).children('caption.title').each(function() {
+        var tabLabel = $(this).html();
+        $(this).remove()
+      });
+      if (tabLabel == null || tabLabel == "") {
+        var tabLabel = tabName.replace("-"," ");
+      }
+      $(this).parent().parent('.content')
+      .addClass("tab-content").siblings('.nav.nav-tabs')
+      .append('<li class="nav-item"><a href=".' + tabName + '-pane" class="nav-link" data-toggle="tab" role="tab" aria-selected="false">' + tabLabel + '</a></li>')
+      $(this).parent().parent('.content').siblings('.nav.nav-tabs').find('.nav-item:first-child a').addClass("active").attr("aria-selected","true")
+      $(this).parent().parent().find('.tab-pane:first-child').addClass("show active")
     });
   });
 });
