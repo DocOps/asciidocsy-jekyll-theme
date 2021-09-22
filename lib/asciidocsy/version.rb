bdbe0bee47c559121ac0a5826ca319f1d91de9b9
Brian Dominick <badominick@gmail.com>
2021-09-21 20:20:34 -0400

    Changes to fix versioning

--- lib/asciidocsy/version.rb
+++ lib/asciidocsy/version.rb
@@ -4,7 +4,9 @@ SafeYAML::OPTIONS[:deserialize_symbols] = true
 SafeYAML::OPTIONS[:default_mode] = :safe
 
 module AsciiDocsy
-  releases = YAML.load_file('lib/asciidocsy/data/releases.yml')
+  spec = Gem::Specification.find_by_name("asciidocsy")
+  gem_root = spec.gem_dir
+  releases = YAML.load_file(gem_root + '/lib/asciidocsy/data/releases.yml')
   if releases['revisions'][0]['patches']
     VERSION = releases['revisions'][0]['patches'][0]['code']
   else
